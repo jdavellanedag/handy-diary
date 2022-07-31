d5986c36767a444399e917fe102de36ea9697b0e
jdavellanedag <jdavellaendag@gmail.com>
2022-07-31 23:26:14 +0200

    Let´s get this party started

---
+++ src/HandyDiaryApp.tsx
@@ -0,0 +1,33 @@
+import "./styles/styles.scss";
+
+export const HandyDiaryApp = () => {
+  return (
+    <>
+      <div>
+        <div>Fecha</div>
+        <div>Clima</div>
+      </div>
+      <div>
+        Tareas
+        <ul>
+          <li>Tarea 1</li>
+          <li>Tarea 2</li>
+          <li>Tarea 3</li>
+          <li>Tarea 4</li>
+          <li>Tarea 5</li>
+        </ul>
+      </div>
+      <div>
+        Prioridades
+        <ul>
+          <li>Tarea 1</li>
+          <li>Tarea 2</li>
+          <li>Tarea 3</li>
+        </ul>
+      </div>
+      <div>Citas</div>
+      <div>Notas</div>
+      <div>Contador de agua</div>
+    </>
+  );
+};
